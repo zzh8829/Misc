5fadf6d418ba8ca1229fb41bc64ccc4beff3b6a2
zzh8829 <zzh8829@gmail.com>
2012-10-23 20:34:24 -0400

    a
    a

---
+++ USACO/Training/3.4/fence4.cpp
@@ -0,0 +1,31 @@
+/*
+ID: zzh8829
+PROG: fence4
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <cstdio>
+using namespace std;
+
+struct P
+{
+	float x,y;
+};
+
+struct L
+{
+	P a,b;
+};
+
+
+int main()
+{
+#ifndef LOCAL
+	ifstream cin("fence4.in");
+	ofstream cout("fence4.out");
+#endif
+	
+	return 0;
+}
--- USACO/Training/3.4/rockers.cpp
+++ USACO/Training/3.4/rockers.cpp
@@ -20,29 +20,16 @@ int main()
 	cin >> N >> T >> M;
 	for(int i=1;i<=N;i++)
 		cin >> m[i];
-	
 	for(int i=1;i<=N;i++)
 	for(int j=1;j<=M;j++)
 	for(int k=1;k<=T;k++)
 	{
 		dp[i][j][k] = max(dp[i-1][j][k],dp[i][j-1][T]);
 		if(m[i]<k)
-		dp[i][j][k] = max(dp[i][j][k],dp[i-1][j][k-m[i]]+1);  
+			dp[i][j][k] = max(dp[i][j][k],dp[i-1][j][k-m[i]]+1);  
 		if(m[i]==k)
-		dp[i][j][k] = max(dp[i][j][k],dp[i-1][j-1][T]+1);  
+			dp[i][j][k] = max(dp[i][j][k],dp[i-1][j-1][T]+1);  
 	}
-	
-	for(int i=1;i<=N;i++)
-	{
-		for(int j=1;j<=M;j++)
-		{
-			for(int k=1;k<=T;k++)
-				cout << dp[i][j][k] <<" ";
-			cout << endl;
-		}
-		cout << endl;
-	}
-	
 	cout << dp[N][M][T] << endl;
 	return 0;
 }
